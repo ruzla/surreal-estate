e226bb23203f5ea0bfa8fd9b248cad0ec4514ba3
Russell <russelldoe@hotmail.co.uk>
2019-04-25 20:28:27 +0100

    created app stylesheet

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import 'raf/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import App from './app';
+import App from './components/app';
 
 render(<App />, document.getElementById('root'));
